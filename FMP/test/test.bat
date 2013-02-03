e6b8273b3cbe428737925e4901fb3c72f63ddbea
bradyhouse <bradyhouse@gmail.com>
2013-02-03 11:47:01 -0600

    Added FMPnb, final netbean version of the project submitted at the beginning of the 2013 spring term.

--- FMP/test/test.bat
+++ FMP/test/test.bat
@@ -1,2 +1,2 @@
 
-java -jar "..\dist\fmp.jar" jdbc:mysql://10.40.81.93:3306/ itm411db itm itm C:\CHProjects\Code\Java\itm411\FMP\data\
+java -jar "..\dist\fmp.jar" jdbc:mysql://10.40.81.93:3306/ itm411db itm itm ..\data\
